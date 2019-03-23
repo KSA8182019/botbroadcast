e0886e85819ccfd5257995974566a525b1f9b061
KSA8182019 <48059633+KSA8182019@users.noreply.github.com>
2019-03-23 20:28:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '$'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`HulixHost`,"http://twitch.tv/S-F")
+client.user.setGame(`Diamond , Shop`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
